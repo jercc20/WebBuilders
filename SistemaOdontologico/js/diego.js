a8ee8b84c50bc7b371afa1a8c3d8cdfa03c73d44
Diego Herrera <diegosoniccolas@hotmail.com>
2013-11-10 03:39:15 -0600

    mejora del js de diego, ahora con popup

--- SistemaOdontologico/js/diego.js
+++ SistemaOdontologico/js/diego.js
@@ -1,5 +1,16 @@
 jQuery(function($) {
 
-	$('#tb-dates').dataTable();
+	if ($("#tb-dates").length > 0) {
+		$('#tb-dates').dataTable();
+	};
+
+	$('.item-remove').on('click', function(){
+		SO.utils.showPopUp( $('#odontograma-popup').html() );
+	});
+
+	$('.popup-process').on('click', function(){
+		SO.utils.showPopUp( $('#process-popup').html() );
+	});
+
 
 });
