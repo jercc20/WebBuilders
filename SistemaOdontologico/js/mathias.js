922e0765eb57a09eb2045add8ba87407afd5d7da
Mathias <mmcpax@gmail.com>
2013-11-10 03:35:50 -0600

    configuracion-consultar-usuarios

--- SistemaOdontologico/js/mathias.js
+++ SistemaOdontologico/js/mathias.js
@@ -3,7 +3,7 @@ jQuery(function($) {
 	$('#tb-dates').dataTable();
 
 	$('.item-remove').on('click', function(){
-		SO.utils.showPopUp( $('#eliminar-usuario-popup').html() );
+		SO.utils.showPopUp( $('#popup-remove').html() );
 });
 
 	$('.btn-accept').on('click', function(){
