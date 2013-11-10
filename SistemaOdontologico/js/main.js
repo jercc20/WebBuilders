182f7526f51bbc27c33d85c702213e7a6c65edd0
Jerson Chinchilla <jercc20@outlook.com>
2013-11-10 03:49:46 -0600

    DataTable Global

--- SistemaOdontologico/js/main.js
+++ SistemaOdontologico/js/main.js
@@ -18,6 +18,24 @@ jQuery(function($) {
 		SO.global.init();
 	}
 
+	//DataTables Init
+	if( $('.data-table').length > 0 ){
+		$('.data-table').dataTable({
+			"bJQueryUI": true,
+			"bLengthChange": false,
+			"sPaginationType": "full_numbers",
+			"oSearch": {},
+			"oLanguage": {
+				"sLengthMenu": "Mostrando _MENU_ resultados por pagina",
+				"sZeroRecords": "No se encontraron resultados",
+				"sInfo": "Mostrando _START_ a _END_ de _TOTAL_ resultados",
+				"sInfoEmpty": "",
+				"sInfoFiltered": "",
+				"sSearch": "Buscar "
+			}
+		});
+	}
+
 	$('a[href="#"]').on('click', function(){ return false; });
 
 });
