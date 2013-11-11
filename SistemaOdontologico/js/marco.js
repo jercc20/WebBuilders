97884fe2e75f7224a59bd57ebb635b428e3f3de6
Jerson Chinchilla <jercc20@outlook.com>
2013-11-10 23:12:26 -0600

    Clening up marco.js
    Were moved to global

--- SistemaOdontologico/js/marco.js
+++ SistemaOdontologico/js/marco.js
@@ -1,16 +1,8 @@
 jQuery(function($) {
 
-	$('.item-remove').on('click', function(){
-		SO.utils.showPopUp( $('#popup-remove').html() );
-});
-
-	$('.btn-accept').on('click', function(){
-		SO.utils.showPopUp('Ha sido eliminado');
-});
-
 	$('.item-view').on('click', function(){
 		SO.utils.showPopUp( $('#popup-bitacora').html() );
-});
+	});
 
 	$('#item-print').on('click', function(){
 		print('#my-table');
