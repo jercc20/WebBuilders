7251593e714a381650c95927b0cd3432470c7649
Jerson Chinchilla <jercc20@outlook.com>
2013-11-11 01:07:45 -0600

    Datepicker spanish

--- SistemaOdontologico/js/main.js
+++ SistemaOdontologico/js/main.js
@@ -48,7 +48,27 @@ jQuery(function($) {
 
 	//Datepicker Init
 	if( $('.datepicker').length > 0 ){
+
+		$.datepicker.regional['es'] = {
+			closeText: 'Cerrar',
+			prevText: '<Ant',
+			nextText: 'Sig>',
+			currentText: 'Hoy',
+			monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+			monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+			dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+			dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+			dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+			weekHeader: 'Sm',
+			dateFormat: 'dd/mm/yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''
+		};
+		$.datepicker.setDefaults($.datepicker.regional['es']);
 		$( ".datepicker" ).datepicker();
+
 	}
 
 	$('a[href="#"]').on('click', function(){ return false; });
