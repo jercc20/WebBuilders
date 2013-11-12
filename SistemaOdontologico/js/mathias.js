d11766f676e8d8f3bf597a8eff37e062db9671a4
Mathias <mmcpax@gmail.com>
2013-11-11 19:25:45 -0600

    crear-consultar-usuarios

--- SistemaOdontologico/js/mathias.js
+++ SistemaOdontologico/js/mathias.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 
 	$('.btn-accept').on( 'submit', function(){
-	SO.utils.showPopUp('Ha sido guardado correctamente.');
+	SO.utils.showPopUp($('.popup-save');
 
-		});
+	});
 });
