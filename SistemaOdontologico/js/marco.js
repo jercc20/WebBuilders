672e3aad63a671b64fed28c501522e4e428bb5a0
Marco Duran <marco_durane@hotmail.com>
2013-11-13 11:08:22 -0600

    Arreglos bitacoras - reportes

--- SistemaOdontologico/js/marco.js
+++ SistemaOdontologico/js/marco.js
@@ -12,10 +12,6 @@ jQuery(function($) {
 		SO.utils.showPopUp( $('#popup-procedure').html() );
 });
 
-	$('#popup').on( 'click', '.btn-aceptar', function(){
-			SO.utils.showPopUp('Ha sido agregado correctamente.');
-});
-
 	$('#item-print').on('click', function(){
 		print('#my-table');
 });
