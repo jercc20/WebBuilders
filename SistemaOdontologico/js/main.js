3eb30a4dc608b4acdb54408bc4d456097e9ae7b3
Jerson Chinchilla <jercc20@outlook.com>
2013-12-14 00:22:17 -0600

    Datepicker global

--- SistemaOdontologico/js/main.js
+++ SistemaOdontologico/js/main.js
@@ -78,27 +78,7 @@ jQuery(function($) {
 
 	//Datepicker Init
 	if( $('.datepicker').length > 0 ){
-
-		$.datepicker.regional['es'] = {
-			closeText: 'Cerrar',
-			prevText: '<Ant',
-			nextText: 'Sig>',
-			currentText: 'Hoy',
-			monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-			monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-			dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-			dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-			dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
-			weekHeader: 'Sm',
-			dateFormat: 'dd-mm-yy',
-			firstDay: 1,
-			isRTL: false,
-			showMonthAfterYear: false,
-			yearSuffix: ''
-		};
-		$.datepicker.setDefaults($.datepicker.regional['es']);
-		$( ".datepicker" ).datepicker();
-
+		SO.global.datepickerInit();
 	}
 
 	//Forms
@@ -135,6 +115,8 @@ jQuery(function($) {
 	});
 	$('#btn-adv-search').on( 'click', function(){
 		SO.utils.showPopUp( $('#popup-adv-search').html() );
+		$('.datepicker').removeClass('hasDatepicker');
+		SO.global.datepickerInit();
 	});
 	$('#popup').on( 'click', '.btn-add', function(){
 		$('#delete-procedures').show();
