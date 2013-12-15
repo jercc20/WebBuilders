0f4e0a51e53f160b36b1cf0d9060cae896a38ff5
Diego Herrera <diegosoniccolas@hotmail.com>
2013-12-14 23:26:04 -0600

    login - check login

---
+++ SistemaOdontologico/includes/check-login.php
@@ -0,0 +1,30 @@
+<?php
+	if( $_POST ){
+		define('PAGE','login');
+		require_once 'functions.php';
+
+		$identificacion = ( isset( $_POST['txt-user'] ) ) ? $_POST['txt-user'] : '';
+		$contrasenna = ( isset( $_POST['psw-login'] ) ) ? md5( $_POST['psw-login'] ) : '';
+
+		$query = "SELECT idUsuario, nombre, idRol FROM tbusuarios WHERE identificacion = '$identificacion' AND contrasenna = '$contrasenna'";
+
+		$result = do_query( $query );
+		if( mysql_num_rows($result) == 1 ){
+
+			$tabla = mysql_fetch_assoc($result);
+
+			$_SESSION['idUsuario'] = $tabla['idUsuario'];
+
+			echo 'Iniciando sesión...';
+			js_redirect('inicio.php', 2500);
+		}else{
+			echo 'Usuario o contraseña incorrectas';
+			js_redirect('login.php',2500);
+		}
+
+		/* ---- */
+
+		global $db_server;
+		mysql_close( $db_server );
+	}
+?>
---
+++ SistemaOdontologico/login.php
@@ -0,0 +1,26 @@
+<?php
+	define('PAGE','login');
+	define('TITLE','Iniciar sesión');
+	$pageConfig = array(
+		'actions' => array(),
+		'plugins'=> array()
+	);
+	require_once 'includes/functions.php';
+	require_once 'includes/header.php';
+
+?>
+	<img id="logo" src="imgs/logo.png" alt="Logo" />
+	<div id="contact-box">
+		<?php configuracion_basica();?>
+	</div>
+	<div id="login-wrap">
+		<h1>Iniciar sesión</h1>
+		<form id="login-form" action="includes/check-login.php" method="post" class="clearfix">
+			<input id="user-name" name="txt-user" type="text" placeholder="Número de identificación o nick" required="required" />
+			<input id="user-psw" name="psw-login" type="password" placeholder="Contraseña" required="required" />
+			<input type="submit" value="Iniciar" />
+		</form>
+	</div>
+<?php
+	require_once 'includes/footer.php';
+?>
