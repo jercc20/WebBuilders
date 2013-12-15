dffc0ecd159ad9d8842ca8cfce1a946e2c81cd86
Diego Herrera <diegosoniccolas@hotmail.com>
2013-12-14 23:27:29 -0600

    php personal

--- SistemaOdontologico/includes/diego.php
+++ SistemaOdontologico/includes/diego.php
@@ -1,20 +1,39 @@
 <?php
 
-	function display_roles_rows(){
+function configuracion_basica(){
 
-		$roles = get_roles();
-		while( $row = mysql_fetch_assoc( $roles ) ){
-			echo '<tr>'.
-				'<td>' . $row['nombreRol'] . '</td>'.
-				'<td><a href="#?id=' . $row['idRol'] . '><i class="icon-edit"></i></a> <a href="#!?idRol=' . $row['idRol'] . '"><i class="icon-remove item-remove"></i></a></td>'.
-			'</tr>';
-		}
+	$tabla = datos_configuracion();
 
+	echo "<p><i class='icon-inicio'></i>".$tabla['direccion']."</p>" .
+	"<p><i class='icon-phone'></i>".$tabla['telefonos']."</p>" .
+	"<p><i class='icon-mail'></i>".$tabla['correoElectronico']."</p>" .
+	"<p><i class='icon-citas'></i>".$tabla['horario']."</p>";
+}
+
+function display_roles_rows(){
+
+	$roles = get_roles();
+	while( $row = mysql_fetch_assoc( $roles ) ){
+		echo '<tr>'.
+			'<td>' . $row['nombreRol'] . '</td>'.
+			'<td><a href="#?id=' . $row['idRol'] . '><i class="icon-edit"></i></a> <a href="#!?idRol=' . $row['idRol'] . '"><i class="icon-remove item-remove"></i></a></td>'.
+		'</tr>';
 	}
+}
 
-	function get_roles(){
-		$query = "SELECT * FROM tbroles";
-		$result = do_query( $query );
-		return $result;
-	}
+function get_roles(){
+	$query = "SELECT * FROM tbroles";
+	$result = do_query( $query );
+	return $result;
+}
+
+function datos_configuracion(){
+	$query = "SELECT * FROM tbconfiguracion WHERE idConfiguracion = '1'";
+
+	$result = do_query($query);
+
+ 	$tabla = mysql_fetch_assoc($result);
+
+ 	return $tabla;
+}
 ?>
