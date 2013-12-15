3b0b1929696e5f8de2328c934a2df3b4a388e379
Mathias <mmcpax@gmail.com>
2013-12-15 16:59:17 -0600

    reporte-editar-bitacoras

--- SistemaOdontologico/includes/mathias.php
+++ SistemaOdontologico/includes/mathias.php
@@ -11,7 +11,7 @@ function display_bitacoras_rows(){
 						echo '<td>' . $fila["o_nombre"] . " ". $fila["o_apellido"] . '</td>';
 						echo '<td>' . $fila["fecha"] . '</td>';
 						echo '<td>' . $fila["procedimiento"] . '</td>';
-						echo '<td><a href="#!?idBitacora=' . $fila['idBitacora'] . '"><i class="icon-edit"></i></a> <i class="icon-remove item-remove"></i></a></td>';
+						echo '<td><a href="editar-bitacora.php?idBitacora=' . $fila['idBitacora'] . '"><i class="icon-edit"></i></a><a href="#!?idCita=' . $fila['idCita'] . '"> <i class="icon-remove item-remove"></i></a></td>';
 					echo '</tr>';
 	}				
 }
@@ -31,7 +31,7 @@ function get_bitacoras(){
 	return $result;
 }
 function display_crear_bitacora_rows(){
-	$crearBitacora = get_crear_cita();
+	$crearBitacora = get_crear_bitacora();
 
 	$fila = mysql_fetch_array($crearBitacora);
 	$nombre = $fila['nombre'];
@@ -53,4 +53,68 @@ function get_crear_bitacora(){
 	$result = do_query( $query );
 	return $result;
 }
+function display_editar_bitacora_rows(){
+	$editarBitacora = get_editar_bitacora();
+
+	$fila = mysql_fetch_array($editarBitacora);
+	$na = $fila['nombre'] . " " . $fila['primerApellido'];
+	$identificacion = $fila['identificacion'];
+
+	echo "<label>Nombre del paciente</label>";
+	echo "<input type='text' name='txt-user-name' readonly value='$na' />";
+	echo "<label>Identificación</label>";
+	echo "<input type='text' name='txt-user-id' readonly value='$identificacion' />";
+}
+
+function get_editar_bitacora(){
+	$id = ( isset( $_GET['idBitacora'] ) ) ? $_GET['idBitacora'] : '';
+	$query = "SELECT * FROM tbbitacoras WHERE idBitacora = $id";
+
+	$result = do_query( $query );
+	return $result;
+}
+function display_reporte_bitacoras_rows(){
+	$bitacoras = get_bitacoras();
+	while( $fila = mysql_fetch_assoc($bitacoras) ){
+		echo '<tr>';  
+			echo '<td>' . $fila["idBitacora"] . '</td>';
+			echo '<td>' . $fila["u_nombre"] . " ".$fila["u_apellido"] . '</td>';
+			echo '<td>' . $fila["u_id"] . '</td>';
+			echo '<td>' . $fila["o_nombre"] . " ". $fila["o_apellido"] . '</td>';
+			echo '<td>' . $fila["fecha"] . '</td>';
+			echo '<td>' . $fila["procedimiento"] . '</td>';
+		echo '</tr>';
+	}
+}
+/*
+function get_bitacoras_custom(){
+	$query = "SELECT * FROM tbbitacoras";
+	if( ! empty( $_POST ) ){
+		if( ! empty( $_POST['txt-patient-id'] ) ||
+			! empty( $_POST['txt-patient-name'] ) ||
+			! empty( $_POST['txt-patient-lastname'] ) ){
+			$query .= " JOIN tbodontogramas USING(idOdontograma)";
+			$query .= " JOIN tbusuarios ON idPaciente = idUsuario";
+		}
+		$query .= " WHERE 1";
+		if( ! empty( $_POST['txt-patient-id'] ) )
+			$query .= " AND identificacion LIKE ('%" . $_POST['txt-patient-id'] . "%')";
+		if( ! empty( $_POST['txt-patient-name'] ) )
+			$query .= " AND nombre LIKE ('%" . $_POST['txt-patient-name'] . "%')";
+		if( ! empty( $_POST['txt-patient-lastname'] ) )
+			$query .= " AND primerApellido LIKE ('%" . $_POST['txt-patient-lastname'] . "%')";
+		if( ! empty( $_POST['txt-start-date'] ) && ! empty( $_POST['txt-end-date'] ) )
+			$query .= " AND fecha BETWEEN '" . do_sql_date_format( $_POST['txt-start-date'] ) . "' AND '" . do_sql_date_format( $_POST['txt-end-date'] ) . "'";
+		elseif( ! empty( $_POST['txt-start-date'] ) )
+			$query .= " AND fecha >= '" . do_sql_date_format( $_POST['txt-start-date'] ) . "'";
+		elseif( ! empty( $_POST['txt-end-date'] ) )
+			$query .= " AND fecha <= '" . do_sql_date_format( $_POST['txt-end-date'] ) . "'";
+	}
+	$result = do_query( $query );
+
+	return $result;
+}
+*/
+
+ 
 ?>
--- SistemaOdontologico/reporte-bitacoras.php
+++ SistemaOdontologico/reporte-bitacoras.php
@@ -30,23 +30,6 @@
 			</table>
 		</div>
 	</div>
-	<div id="popup-up" class="hide">
-		<form class="clearfix" action="" method="post">
-			<section class="form-section">
-				<label for="dentist-name">Nombre del odontólogo: </label>
-				<input id="dentist-name" name="txt-dentist-name" type="text" />
-				<label for="patient-name">Nombre del paciente: </label>
-				<input id="patient-name" name="txt-patient-name" type="text" />
-				<label for="start-date">Fecha de inicio: </label>
-				<input id="start-date" name="txt-start-date" type="text" placeholder="dd-mm-yyyy" class="datepicker" pattern="(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d" />
-				<label for="end-date">Fecha de fin: </label>
-				<input id="end-date" name="txt-end-date" type="text" placeholder="dd-mm-yyyy" class="datepicker" pattern="(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d" />
-				<div class="ac cb">
-					<button class="item-accept">Aceptar</button>
-					<button class="close">Cancelar</button>
-				</div>
-			</section>
-		</form>
 <?php
 	require_once 'includes/footer.php';
 ?>
