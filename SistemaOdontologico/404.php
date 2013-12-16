bdabb01a2406bbdb95a62734e1839ff44f34890f
jercc20 <jercc20@outlook.com>
2013-12-16 12:49:26 -0600

    404 page

---
+++ SistemaOdontologico/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<title>No se ha encontrado la página</title>
+	<style>
+		body {
+			padding-top: 100px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<img src="imgs/404.jpg" alt="404" />
+</body>
+</html>
