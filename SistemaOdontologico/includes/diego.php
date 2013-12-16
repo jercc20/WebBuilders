8e57f1d6eec11049d3405a358d48b39035245966
Diego Herrera <diegosoniccolas@hotmail.com>
2013-12-15 22:17:45 -0600

    reporte de usuarios

--- SistemaOdontologico/includes/diego.php
+++ SistemaOdontologico/includes/diego.php
@@ -21,6 +21,66 @@ function display_roles_rows(){
 	}
 }
 
+function display_reporte_usuarios_rows(){
+
+	$usuarios = ( ! empty( $_POST ) ) ? get_usuarios_custom() : get_usuarios();
+
+	while ( $row = mysql_fetch_assoc( $usuarios ) ) {
+
+		if (isset($row['nombreRol'])) {
+			$nombreRol = $row['nombreRol'];
+		}else{
+			$result = get_rol_usuario($row['idRol']);
+			$tabla = mysql_fetch_assoc($result);
+
+			$nombreRol = $tabla['nombreRol'];
+		}
+
+		echo '<tr>';
+			echo '<td>' . $row["nombre"]." ".$row["primerApellido"]." ".$row["segundoApellido"] . '</td>';
+			echo '<td>' . $row["identificacion"] . '</td>';
+			echo '<td>' . $nombreRol . '</td>';
+			echo '<td>' . $row["telefonoCasa"] .' / '.$row["telefonoCelular"]. '</td>';
+			echo '<td>' . $row["correoElectronico"] . '</td>';
+			echo '<td>' . $row["domicilio"] . '</td>';
+		echo '</tr>';
+	}
+}
+
+function get_usuarios_custom(){
+	$query = "SELECT * FROM tbusuarios";
+	if( ! empty( $_POST ) ){
+		$query .= " WHERE 1";
+
+		if( ! empty( $_POST['user-id'] ) )
+			$query .= " AND identificacion LIKE ('%" . $_POST['user-id'] . "%')";
+		if( ! empty( $_POST['txt-user-name'] ) )
+			$query .= " AND nombre LIKE ('%" . $_POST['txt-user-name'] . "%')";
+		if( ! empty( $_POST['txt-user-lastname'] ) )
+			$query .= " AND primerApellido LIKE ('%" . $_POST['txt-user-lastname'] . "%')";
+		if ( ! empty( $_POST['user-rol'] ) && $_POST['user-rol'] !=0 )
+			$query .= " AND idRol = " . $_POST['user-rol'] . "";
+	}
+	$result = do_query( $query );
+
+	return $result;
+}
+
+function get_rol_usuario($pidRol){
+	$query = "SELECT nombreRol FROM tbroles WHERE idRol = '$pidRol'";
+	$result = do_query($query);
+
+	return $result;
+}
+
+function display_array_rol(){
+	$roles = get_roles();
+	echo '<option value="0">Todos</option>';
+	while ($fila = mysql_fetch_row($roles)){
+	    	echo '<option value='. $fila["0"] .'>'. $fila["1"]. '</option>';
+	}
+}
+
 function get_roles(){
 	$query = "SELECT * FROM tbroles";
 	$result = do_query( $query );
---
+++ SistemaOdontologico/reporte-usuarios.php
@@ -0,0 +1,53 @@
+<?php
+	define('PAGE','reporte-usuarios'); //nombre de la pagina
+	define('TITLE','Reporte de usuarios'); //titulo de la pagina
+	$pageConfig = array(
+		'actions' => array(), //array con las acciones adicionales de la pagina (editar, borrar, etc)php
+		'plugins'=> array('datatable') //para incluir archivos de plugins (datatable, calendar, datepicker, print, etc)
+	);
+	require_once 'includes/functions.php';
+	require_once 'includes/header.php';
+?>
+	<h1 class="ac">Reporte Usuarios</h1>
+	<button id="btn-adv-search">Búsqueda Avanzada</button>
+	<a id="print" href="#"><i class="icon-print fr"></i></a>
+	<table id="my-table" class="data-table display">
+		<thead>
+			<tr>
+				<th>Nombre de usuario</th>
+				<th>Identificación</th>
+				<th>Rol</th>
+				<th>Teléfono</th>
+				<th>Correo</th>
+				<th>Dirección</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php display_reporte_usuarios_rows(); ?>
+		</tbody>
+	</table>
+	<div id="popup-adv-search" class="hide">
+		<form class="clearfix" action="" method="post">
+			<section class="form-section ac">
+				<label for="user-id">Identificación: </label>
+				<input id="user-id" name="user-id" type="text" />
+				<label for="user-name"> Nombre: </label>
+				<input id="user-name" name="txt-user-name" type="text" />
+				<label for="user-lastname">Primer apellido: </label>
+				<input id="user-lastname" name="txt-user-lastname" type="text"/>
+				<label for="user-rol">Rol: </label>
+				<select id="user-rol" name="user-rol">
+					<?php display_array_rol(); ?>
+				</select>
+			</section>
+			<section>
+				<div class="ac cb">
+					<button class="item-accept">Aceptar</button>
+					<button class="close">Cancelar</button>
+				</div>
+			</section>
+		</form>
+	</div>
+<?php
+	require_once 'includes/footer.php';
+?>
