7c40b9ce103d18652eedd0d0143432ffa5402de9
johelaq <johelaq@gmail.com>
2013-12-15 22:26:20 -0600

    Editar usuario

---
+++ SistemaOdontologico/editar-usuario.php
@@ -0,0 +1,24 @@
+<?php
+	define('PAGE','editar-usuario'); //nombre de la pagina
+	define('TITLE','Editar Usuario'); //titulo de la pagina
+	$pageConfig = array(
+		'plugins'=> array('datepicker') //para incluir archivos de plugins (datatable, calendar, datepicker, print, etc)
+	);
+	require_once 'includes/functions.php';
+	require_once 'includes/header.php';
+?>
+	<h1 class="ac">Editar Usuario</h1>
+	<form id="proced-box" class="form-edit" action="includes/update-usuarios.php" method="post">
+		<section class="form-section">
+			<?php
+				display_edit_usuarios_rows();
+			?>
+		</section>
+		<div class="ac cb">
+			<button class="form-cancel">Cancelar</button>
+			<button type="submit">Guardar</button>
+		</div>
+	</form>
+<?php
+	require_once 'includes/footer.php';
+?>
---
+++ SistemaOdontologico/includes/update-usuarios.php
@@ -0,0 +1,35 @@
+<?php
+	if( $_POST ){
+		define('PAGE','editar-usuarios');
+		require_once 'functions.php';
+		$id = ( isset( $_POST['idUsuario'] ) ) ? $_POST['idUsuario'] : '';
+		$name = ( isset( $_POST['txt_name'] ) ) ? $_POST['txt_name'] : '';
+		$lastname = ( isset( $_POST['txt_lastname'] ) ) ? $_POST['txt_lastname'] : '';
+		$lastname2 = ( isset( $_POST['txt_lastname2'] ) ) ? $_POST['txt_lastname2'] : '';
+		$userId = ( isset( $_POST['txt_user'] ) ) ? $_POST['txt_user'] : '';
+		$housePhn = ( isset( $_POST['txt_phone'] ) ) ? $_POST['txt_phone'] : '';
+		$CellPhn = ( isset( $_POST['txt_cellphone'] ) ) ? $_POST['txt_cellphone'] : '';
+		$email = ( isset( $_POST['txt_email'] ) ) ? $_POST['txt_email'] : '';
+		$birth = ( isset( $_POST['txt_birthday'] ) ) ? $_POST['txt_birthday'] : '';
+		$birthSql = do_sql_date_format($birth);
+		$impmt = ( isset( $_POST['txt_impairment'] ) ) ? $_POST['txt_impairment'] : '';
+		$role = ( isset( $_POST['slt-user-role'] ) ) ? $_POST['slt-user-role'] : '';
+		$alergie  = ( isset( $_POST['txt-user-alergie'] ) ) ? $_POST['txt-user-alergie'] : '';
+		$UserAdrs = ( isset( $_POST['txt-user-adress'] ) ) ? $_POST['txt-user-adress'] : '';
+		
+		$query = "UPDATE tbusuarios SET nombre = '$name', primerApellido = '$lastname', segundoApellido = '$lastname2', identificacion = '$userId', 
+						 telefonoCasa = '$housePhn', telefonoCelular = '$CellPhn', correoElectronico = '$email', fechaNacimiento = '$birthSql', discapacidad = '$impmt', 
+						 idRol = '$role', alergias = '$alergie', domicilio = '$UserAdrs'";
+
+		if( ! empty( $_POST['txt-user-psw'] ) ){
+			$userPsw = md5($_POST['txt-user-psw']);
+			$query .= ", contrasenna = '$userPsw'";
+		}
+				
+		$query .= " WHERE idUsuario = '$id'";
+		echo do_query( $query );
+
+		global $db_server;
+		mysql_close( $db_server );
+	}
+?>
