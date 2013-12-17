da863d7de001f762458ae3a4bfe63c740771e614
Diego Herrera <diegosoniccolas@hotmail.com>
2013-12-17 01:28:08 -0600

    editar el perfil, falta una validacion

---
+++ SistemaOdontologico/editar-perfil-usuario.php
@@ -0,0 +1,72 @@
+<?php
+	define('PAGE','editar-perfil-usuario');
+	define('TITLE','Editar el perfil del usuario');
+	$pageConfig = array(
+		'actions' => array(),
+		'plugins'=> array('datepicker')
+	);
+	require_once 'includes/functions.php';
+	require_once 'includes/header.php';
+
+	$idUsuario = $_SESSION['idUsuario'];
+
+	$tabla = get_perfil_user($idUsuario);
+?>
+	<h1 class="ac">Editar Perfil</h1>
+	<form class="form-edit box-wrap clearfix" action="includes/update-perfil-usuario.php" method="get">
+		<section class="form-section fl">
+			<label for="user-id">Identificación</label>
+			<input id="user-id" name="txt-user-id" type="text" readonly="readonly" <?php echo 'value="'. $tabla["identificacion"] .'"' ?> />
+			<label for="user-name">Nombre</label>
+			<input id="user-name" name="txt-user-name" type="text" required="required" <?php echo 'value="'. $tabla["nombre"] .'"' ?>  pattern="[A-Za-z]+" />
+			<label for="user-lastname">Apellido</label>
+			<input id="user-lastname" name="txt-user-lastname" type="text" required="required" <?php echo 'value="'. $tabla["primerApellido"] .'"' ?> />
+			<label for="user-lastname2">Segundo apellido</label>
+			<input id="user-lastname2" name="txt-user-lastname2" type="text" value="Campos" <?php if($tabla["segundoApellido"]) echo 'value="'. $tabla["segundoApellido"] .'"' ?>  />
+			<label for="user-phone">Teléfono de la casa</label>
+			<input id="user-phone" name="txt-user-phone" type="text" <?php echo 'value="'. $tabla["telefonoCasa"] .'"' ?> pattern="[\d]{8,10}" placeholder="22222222" />
+			<label for="user-cellphone">Teléfono celular</label>
+			<input id="user-cellphone" name="txt-user-cellphone" type="text" <?php if($tabla["telefonoCelular"]) echo 'value="'. $tabla["telefonoCelular"] .'"' ?> pattern="[0-9]{8,10}" placeholder="88888888" />
+			<label for="user-email">Correo electrónico</label>
+			<input id="user-email" name="txt-user-email" type="text" <?php echo 'value="'. $tabla["correoElectronico"] .'"' ?> />
+			<label for="user-dob">Fecha de nacimiento</label>
+			<input id="user-dob" name="txt-user-dob" type="text" required="required" placeholder="dd/mm/yyyy" class="datepicker" <?php echo 'value="'. $tabla["fechaNacimiento"] .'"' ?> />
+		</section>
+		<section class="form-section fr">
+			<label for="user-psw">Contraseña actual</label>
+			<input id="user-psw" name="psw-user" type="password" />
+			<label for="user-pnew">Contraseña nueva</label>
+			<input id="user-pnew" name="psw-user-pnew" type="password" />
+			<label for="user-pnew2">Repetir contraseña nueva</label>
+			<input id="user-pnew2" name="psw-user-pnew2" type="password" />
+			<label for="user-address">Domicilio</label>
+			<textarea id="user-address" name="txt-user-address"><?php echo $tabla["domicilio"] ?></textarea>
+		</section>
+		<section class="cb ac">
+			<input type="submit" id="btnEnviar" value="Guardar" />
+		</section>
+	</form>
+<?php
+	require_once 'includes/footer.php';
+?>
+<script type="text/javascript">
+	document.getElementById('btnEnviar').addEventListener('click', validar);
+
+	function validar (e){
+		var txtContrasennaActual = document.getElementById('user-psw'),
+		txtContrasennaNueva = document.getElementById('user-pnew'),
+		txtContrasennaNueva2 = document.getElementById('user-pnew2');
+
+		if (txtContrasennaNueva.length > 0) {
+			if (txtContrasennaActual.length > 0) {
+				if (! (txtContrasennaNueva.value === txtContrasennaNueva2.value)) {
+					SO.utils.showPopUp('Las contraseñas no son iguales');
+					e.preventDefault();
+				}
+			}else{
+				SO.utils.showPopUp('Debe ingresar la contraseña actual');
+				e.preventDefault();
+			}
+		}
+	}
+</script>
---
+++ SistemaOdontologico/includes/update-perfil-usuario.php
@@ -0,0 +1,56 @@
+<?php
+	if( $_POST ){
+		define('PAGE','editar-perfil-usuario'); //mismo nombre de la pagina del formulario
+		require_once 'functions.php';
+
+		$name = ( isset( $_POST['txt-user-name'] ) ) ? $_POST['txt-user-name'] : '';
+		$lastname = ( isset( $_POST['txt-user-lastname'] ) ) ? $_POST['txt-user-lastname'] : '';
+		$lastname2 = ( isset( $_POST['txt-user-lastname2'] ) ) ? $_POST['txt-user-lastname2'] : '';
+		$userId = ( isset( $_POST['txt-user-id'] ) ) ? $_POST['txt-user-id'] : '';
+		$housePhn = ( isset( $_POST['txt-user-phone'] ) ) ? $_POST['txt-user-phone'] : '';
+		$CellPhn = ( isset( $_POST['txt-user-cellphone'] ) ) ? $_POST['txt-user-cellphone'] : '';
+		$email = ( isset( $_POST['txt-user-email'] ) ) ? $_POST['txt-user-email'] : '';
+		$birth = ( isset( $_POST['txt-user-dob'] ) ) ? $_POST['txt-user-dob'] : '';
+		$birthSql = do_sql_date_format($birth);
+		$domicilio = ( isset( $_POST['txt-user-address'] ) ) ? $_POST['txt-user-address'] : '';
+		$contrasennaNueva = ( isset( $_POST['psw-user-pnew'] ) ) ? md5($_POST['psw-user-pnew']) : '';
+
+		print_r( $_POST );
+
+		$query = "UPDATE tbusuarios SET nombre = '$name', primerApellido = '$lastname', segundoApellido = '$lastname2', telefonoCasa = '$housePhn', telefonoCelular = '$CellPhn', correoElectronico = '$email', fechaNacimiento = '$birthSql', domicilio = '$domicilio'";
+
+		if (! empty($_POST['psw-user'] ) ) {
+
+			$contrasenna = md5($_POST['psw-user']);
+			$query2 = "SELECT contrasenna FROM tbusuarios WHERE identificacion = '$userId'";
+			$result = do_query($query2);
+			$tabla = mysql_fetch_assoc($result);
+
+			$contrasennaCom = $tabla['contrasenna'];
+
+			if ( ! strcmp($contrasenna, $contrasennaCom)) {
+
+				$query .= ", contrasenna = '$contrasennaNueva'";
+
+			}else{
+				echo 'Contraseña actual incorrecta';
+				exit();
+			}
+		}
+
+		$query .= " WHERE identificacion = '$userId'";
+
+		echo $query;
+
+		$result = do_query( $query );
+		if( $result == 1 ){
+			echo 'Perfil modificado con exito';
+			js_redirect('editar-perfil-usuario.php', 4000);
+		}
+
+		/* ---- */
+
+		global $db_server;
+		mysql_close( $db_server );
+	}
+?>
