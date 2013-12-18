fd939a4ceeae2933be5ee1763b2e9a44f6d53505
Jerson Chinchilla <jercc20@outlook.com>
2013-12-18 03:54:31 -0600

    Get events updated

--- SistemaOdontologico/includes/get-events.php
+++ SistemaOdontologico/includes/get-events.php
@@ -2,26 +2,25 @@
 define('PAGE','consultarCalendario');
 require_once 'functions.php';
 
- $json = array();
-
- $query = "SELECT c.*, p.nombre AS p_nombre,
+$query = "SELECT c.*, p.nombre AS p_nombre,
 				p.primerApellido AS p_apellido,
 				p.identificacion AS p_id
 			FROM tbcitas AS c
 			LEFT JOIN tbusuarios AS p ON (p.idUsuario = c.idPaciente)";
 
- $result = do_query($query);
+$result = do_query($query);
+$citas = array();
+$k = 0;
 
- $citas = array();
- 
- foreach (mysql_fetch_assoc($result) AS $k => $item) {
- 	$citas[$k]['title'] = $item['hora'] . ' ' . $item['p_nombre'];
- 	$citas[$k]['start'] = '2013-12-03 00:00:00';
- 	$citas[$k]['end'] = '2013-12-03 00:00:00';
- }
- echo json_encode($citas);
+while( $row = mysql_fetch_assoc( $result ) ){
+	$citas[$k]['title'] = $row['hora'] . ' ' . $row['p_nombre'];
+	$citas[$k]['start'] = $row['fecha'];
+	$citas[$k]['end'] = $row['fecha'];
+	$k++;
+}
+echo json_encode($citas);
 
- global $db_server;
- mysql_close( $db_server );
- 
+global $db_server;
+mysql_close( $db_server );
+
 ?>
