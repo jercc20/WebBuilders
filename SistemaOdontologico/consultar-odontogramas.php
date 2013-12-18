5feb8165966c4331224fbedad048503f7b2456ac
Lausj <laura.salazarj@gmail.com>
2013-12-17 23:29:48 -0600

    consultar-odontogramas

--- SistemaOdontologico/consultar-odontogramas.php
+++ SistemaOdontologico/consultar-odontogramas.php
@@ -11,16 +11,16 @@
 ?>
         <h1 class="ac">Consultar Odontogramas</h1>
         <div id="add-user">
-                <a  class="fr" href="crear-odontograma.php">+ agregar odontograma</a>
+                <a href="select-usuario.php?url=crear-odontograma.php">+Agregar Odontograma</a>
         </div>
         <table class="data-table display">
                 <thead>
                         <tr>
                                 <th>Número de odontograma</th>
-                                <th data-field="txt-pacient-name">Nombre del paciente</th>
-                                <th data-field="txt-pacient-id">Identificación del paciente</th>
-                                <th data-field="txt-dentist-name">Nombre del odontólogo</th>
-                                <th data-field="txt-date-realized">Última Fecha realizada</th>
+                                <th>Nombre del paciente</th>
+                                <th>Identificación del paciente</th>
+                                <th>Nombre del odontólogo</th>
+                                <th>Última Fecha realizada</th>
                                 <th>Número de procedimientos</th>
                                 <th>Costo total</th>
                                 <th class="column-icons"></th>
