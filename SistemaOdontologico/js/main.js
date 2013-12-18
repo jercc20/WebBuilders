4255411f913dc9753195da7a6e1295fe5efd0cb3
Jerson Chinchilla <jercc20@outlook.com>
2013-12-18 03:25:01 -0600

    Fullcalendar config

--- SistemaOdontologico/js/main.js
+++ SistemaOdontologico/js/main.js
@@ -81,42 +81,20 @@ jQuery(function($) {
 	}
 
 	//Fullcalendar
-	/*if( $("#calendar").length > 0 ){
+	if( $("#calendar").length > 0 ){
 
 		var date = new Date();
 		var d = date.getDate();
 		var m = date.getMonth();
 		var y = date.getFullYear();
 		var calendar = $('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-			selectable: true,
-			selectHelper: true,
-			select: function(start, end, allDay) {
-				var title = prompt('Event Title:');
-				if (title) {
-					calendar.fullCalendar('renderEvent',
-						{
-							title: title,
-							start: start,
-							end: end,
-							allDay: allDay
-						},
-						true // make the event "stick"
-					);
-				}
-				calendar.fullCalendar('unselect');
-			},
-			editable: true,
+			events: "events.php",
 			dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie','Jue', 'Vie', 'Sab'],
 			dayNames: ['Domingo', 'Lunes', 'Martes', 'Miercoles','Jueves', 'Viernes', 'Sabado'],
 			monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
 			monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-			});
-	};*/
+		});
+	}
 
 	//Datepicker Init
 	if( $('.datepicker').length > 0 ){
