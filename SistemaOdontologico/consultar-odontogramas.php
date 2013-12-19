55577cc4e8b6ad0404eb8f7af3b97784161dd1b5
Lausj <laura.salazarj@gmail.com>
2013-12-19 00:01:09 -0600

    varios

--- SistemaOdontologico/consultar-odontogramas.php
+++ SistemaOdontologico/consultar-odontogramas.php
@@ -11,20 +11,20 @@
 ?>
         <h1 class="ac">Consultar Odontogramas</h1>
         <div id="add-user">
-                <a href="select-usuario.php?url=crear-odontograma.php">+Agregar Odontograma</a>
+                <a class="fr" href="select-usuario.php?url=crear-odontograma.php">+Agregar Odontograma</a>
         </div>
         <table class="data-table display">
                 <thead>
-                        <tr>
-                                <th>Número de odontograma</th>
-                                <th>Nombre del paciente</th>
-                                <th>Identificación del paciente</th>
-                                <th>Nombre del odontólogo</th>
-                                <th>Última Fecha realizada</th>
-                                <th>Número de procedimientos</th>
-                                <th>Costo total</th>
-                                <th class="column-icons"></th>
-                        </tr>
+                <tr>
+                        <th>Número de odontograma</th>
+                        <th>Nombre del paciente</th>
+                        <th>Identificación del paciente</th>
+                        <th>Nombre del odontólogo</th>
+                        <th>Última Fecha realizada</th>
+                        <th>Número de procedimientos</th>
+                        <th>Costo total</th>
+                        <th class="column-icons"></th>
+                </tr>
                 </thead>
                 <tbody>
                         <?php display_odontogramas_rows(); ?>
--- SistemaOdontologico/includes/laura.php
+++ SistemaOdontologico/includes/laura.php
@@ -101,10 +101,10 @@ function display_add_procedimientos_popup() {
 		$i = 0;
 		while ($fila = mysql_fetch_assoc($procedimientos)) {
 			echo '<tr>';
-				echo '<td>' . '<input type="checkbox" name="procedure" />' . '<input type="hidden" name="procedimientos['. $i .']" value="'. $fila["idProcedimiento"] .'" /></td>';
+				echo '<td>' . '<input type="checkbox" name="procedure" />' . '<input type="hidden" name="procedimientos['. $i .']" value="'. $fila["idProcedimiento"] .'" />';
 				echo '<td>' . $fila["nombre"] .'</td>';
 				echo '<td>' . $fila["Costo"] .'</td>';
-				echo '<td class="has-zone"><input type="text​" name="zona['. $i .']" placeholde​r="Zona" required="​required" /></td>';
+				echo '<td class="has-zone"><input type="text​" name="zone​['. $i .']" placeholde​r="Zona" required="​required" /></td>';
 			echo '</tr>';
 			$i++;
 		}
@@ -165,7 +165,7 @@ function display_reporte_odontogramas_rows(){
 	$odontograma = ( ! empty( $_POST ) ) ? get_odontograma_custom() : get_odontograma();
 	while( $fila = mysql_fetch_assoc($odontograma) ){
 
-		echo '<tr>';
+		echo '<tr>';  
 			echo '<td>' . $fila["idOdontograma"] . '</td>';
 			echo '<td>' . $fila["u_nombre"] . " ".$fila["u_apellido"] . '</td>';
 			echo '<td>' . $fila["u_id"] . '</td>';
@@ -197,9 +197,9 @@ function get_odontograma_custom(){
 }
 
 function get_odontograma(){
-	$query = "SELECT b.*,
-			u.nombre AS u_nombre,
-			o.nombre AS o_nombre,
+	$query = "SELECT b.*, 
+			u.nombre AS u_nombre, 
+			o.nombre AS o_nombre, 
 			o.primerApellido AS o_apellido,
 			u.primerApellido AS u_apellido,
 			u.identificacion AS u_id
@@ -210,6 +210,57 @@ function get_odontograma(){
 	$result=do_query($query);
 	return $result;
 }
-?>
+################################################################################################################################
+
+function display_reporte_procedimientos_rows(){
+
+	$procedimiento = ( ! empty( $_POST ) ) ? get_procedimiento_custom() : get_procedimiento();
+	while( $fila = mysql_fetch_assoc($procedimiento) ){
+
+		echo '<tr>';  
+			echo '<td>' . $fila["idProcedimiento"] . '</td>';
+			echo '<td>' . $fila["u_nombre"] . " ".$fila["u_apellido"] . '</td>';
+			echo '<td>' . $fila["u_id"] . '</td>';
+			echo '<td>' . $fila["o_nombre"] . " ". $fila["o_apellido"] . '</td>';
+			echo '<td>' . $fila["fecha"] . '</td>';
+			echo '<td>' . $fila["procedimiento"] . '</td>';
+		echo '</tr>';
+
+	}
+}
+
+function get_procedimiento_custom(){
+	$query = "SELECT * FROM tbprocedimientos order by id";
+	if( ! empty( $_POST ) ){
+		$query .= " WHERE 1";
+
+		if( ! empty( $_POST['user-id'] ) )
+			$query .= " AND identificacion LIKE ('%" . $_POST['user-id'] . "%')";
+		if( ! empty( $_POST['txt-user-name'] ) )
+			$query .= " AND nombre LIKE ('%" . $_POST['txt-user-name'] . "%')";
+		if( ! empty( $_POST['txt-user-lastname'] ) )
+			$query .= " AND primerApellido LIKE ('%" . $_POST['txt-user-lastname'] . "%')";
+		if ( ! empty( $_POST['user-rol'] ) && $_POST['user-rol'] !=0 )
+			$query .= " AND idRol = " . $_POST['user-rol'] . "";
+	}
+	$result = do_query( $query );
+
+	return $result;
+}
+
+function get_procedimiento(){
+	$query = "SELECT b.*, 
+			u.nombre AS u_nombre, 
+			o.nombre AS o_nombre, 
+			o.primerApellido AS o_apellido,
+			u.primerApellido AS u_apellido,
+			u.identificacion AS u_id
+					FROM tbbitacoras AS b
+					LEFT JOIN tbusuarios AS u ON (u.idUsuario = b.idPaciente)
+					LEFT JOIN tbusuarios AS o ON (o.idUsuario = b.idOdontologo)";
+
+	$result=do_query($query);
+	return $result;
+}
 
 ?>
